3b22f8c92c710c48667895aceea1275ae0578bcf
Rakesh Bag <150882390+Rakeshbag7@users.noreply.github.com>
2025-08-25 13:02:03 +0530

    Add files via upload

---
+++ Problem Dayy 18 Subset sum problem.py
@@ -0,0 +1,16 @@
+n, target = map(int, input().split())
+nums = list(map(int, input().split()))
+
+dp = [False] * (target + 1)
+dp[0] = True
+
+for num in nums:
+    for j in range(target, num - 1, -1):
+        dp[j] = dp[j] or dp[j - num]
+
+if dp[target]:
+    result = "YES"
+    print(result)
+else:
+    result = "NO"
+    print(result)
